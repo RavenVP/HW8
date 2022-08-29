ac491d763c536087186ab67d51745803c14d4be5
RavenVP <Raven_vp_72@mail.ru>
2022-08-29 20:36:56 +0500

    Выполнил HW8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hel lo world!");
+        isYearLeap(2020);
+        downloadText(1, 2020);
+        deliveryRangeDistance(1012);
+
     }
+    // Задание 1 \\
+    public static int isYearLeap(int year) {
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " - високосный год ");
+        } else {
+            System.out.println(year + " - не високосный год ");
+        }
+        return year;
+    }
+    // Задание 2 \\
+    public static void downloadText(int clientOS, int clientDeviceYear) {
+
+        int currentYear = LocalDate.now().getYear();
+
+        if (clientDeviceYear <= currentYear && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientDeviceYear > currentYear && clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientDeviceYear <= currentYear && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear > currentYear && clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+    // Задание 3 \\
+    public static void deliveryRangeDistance (int deliveryDistance){
+        int deliveryDays = 1;
+        int differenceDays = 40;
+        int startLimit = 20;
+        if ( deliveryDistance <= 20){
+            System.out.println("Потребуется дней:" + deliveryDays);
+        }
+        else if (deliveryDistance > 20 && deliveryDistance <=59){
+            deliveryDays = deliveryDays + 1;
+            System.out.println("Потребуется дней:" + deliveryDays);
+        }
+        else if (deliveryDistance > 59){
+            deliveryDays = (deliveryDistance + startLimit) / differenceDays + 1;
+            System.out.println("Потребуется дней:" + deliveryDays);
+        }
+    }
+
 }
